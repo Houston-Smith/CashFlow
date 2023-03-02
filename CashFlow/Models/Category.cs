f875bc1a863041e05a6f1b42a297acdbfd3b45c6
Houston-Smith <houstonbsmith@gmail.com>
2023-03-02 14:34:37 -0600

    Setup and Models

---
+++ CashFlow/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
---
+++ CashFlow/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CashFlow.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Ammount { get; set; }
+
+        public string Note { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserProfileId { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
