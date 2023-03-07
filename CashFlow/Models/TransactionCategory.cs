ca0d6b7297b6b0fbd088a742d5dea6a675df1777
Houston-Smith <houstonbsmith@gmail.com>
2023-03-06 21:36:35 -0600

    TransactionCategory Model

---
+++ CashFlow/Models/TransactionCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CashFlow.Models
+{
+    public class TransactionCategory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int TransactionId { get; set; }
+
+        [Required]
+        public int CateagoryId { get; set;} 
+    }
+}
