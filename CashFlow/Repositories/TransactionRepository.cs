aaf90de09444157f7c10dcc95f3cc5ed0c6807fd
Houston-Smith <houstonbsmith@gmail.com>
2023-03-07 06:25:25 -0600

    GetTransactionsByUserId

--- CashFlow/Repositories/TransactionRepository.cs
+++ CashFlow/Repositories/TransactionRepository.cs
@@ -123,6 +123,62 @@ namespace CashFlow.Repositories
                 }
             }
         }
+
+       public List<Transaction> GetTransactionsByUserId(int userId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT t.Id AS TransactionId, t.Ammount, t.Note, t.Date, t.UserProfileId, t.Category,
+                        tc.Id AS TransactionCategoryId,
+                        c.Id AS CategoryId, c.Name, c.Type
+
+                        FROM Transaction t
+                        LEFT JOIN TransactionCategory tc ON t.Id = tc.TransactionId
+                        LEFT JOIN Category c ON tc.CategoryId = c.Id
+                        WHERE UserProfileId = @UserProfileId
+                        ";
+
+                    cmd.Parameters.AddWithValue("@UserProfileId", userId);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var transactions = new List<Transaction>();
+                        while (reader.Read())
+                        {
+                            var transactionId = DbUtils.GetInt(reader, "TransactionId");
+                            var existingTransaction = transactions.FirstOrDefault(p = prop.Id == transactionId);
+                            if (existingTransaction == null)
+                            {
+                                existingTransaction = new Transaction()
+                                {
+                                    Id = transactionId,
+                                    Ammount = DbUtils.GetInt(reader, "Ammount"),
+                                    Note = DbUtils.GetString(reader, "Note"),
+                                    Date = DbUtils.GetDateTime(reader, "Date"),
+                                    UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                                    Categories = new List<Category>()
+                                };
+                                transactions.Add(existingTransaction);
+                            }
+                            if (DbUtils.IsNotDbNull(reader, "TransactionId"))
+                            {
+                                existingTransaction.Categories.Add(new Category()
+                                {
+                                    Id = DbUtils.GetInt(reader, "CategoryId"),
+                                    Name = DbUtils.GetString(reader, "Name"),
+                                    Type = DbUtils.GetString(reader, "Type"),
+                                });
+                            }
+                        }
+
+                        return transactions;
+                    }
+                }
+            }
+        }
     }
 }
 
